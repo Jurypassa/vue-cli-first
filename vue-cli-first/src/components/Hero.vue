6336f32b7cfa72d09757eba2d0f94a33de2770fe
Jury <jurypassa@gmail.com>
2021-12-10 16:31:50 +0100

    Bonus

---
+++ vue-cli-first/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Hero"></div>
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
+
+
+<style scoped>
+
+    #Hero{
+        background-image: url(https://i1.wp.com/www.comicsuniverse.it/wp-content/uploads/2018/02/27788128_1690518157691965_7916507482103323702_o.jpg);
+        width: 100%;
+        height: 500px;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position:center;
+    }
+</style>
